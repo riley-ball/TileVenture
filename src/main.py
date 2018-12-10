a72aabfcd1c3743ba3ee4f8004d31f38ad47224a
Riley PC <riley-ball@outlook.com>
2018-12-10 20:13:09 +1000

    inital commit

--- src/main.py
+++ src/main.py
@@ -29,6 +29,9 @@ class GameApp(object):
         self.refresh_view(5)
 
         view.bind_all("<Key>", self._key_press)
+        view.bind_all("<KeyPress>", self.keydown)
+        view.bind_all("<KeyRelease>", self.keyup)
+        self._key_flag = False
 
     def _setup_game(self):
         self._view.generate_map()
@@ -40,41 +43,53 @@ class GameApp(object):
         self._view.draw_player(self._player_grid_pos,
                                self._player_direction, draw_flag)
 
+    def keyup(self, e):
+        self._key_flag = False
+        print('up', e.char, e.type)
+
+    def keydown(self, e):
+        self._key_flag = True
+        self._key_press(e)
+
     def _key_press(self, event):
         key = event.keysym
+        while self._key_flag:
+            if key == "Up":
+                print(event.type,  "aaaaa")
+                new_coords = (
+                    self._player_grid_pos[0], self._player_grid_pos[1]-1)
+                if self._player_grid_pos[1]-1 >= 0:
+                    self._player_grid_pos = new_coords
+                    self._player_direction = 'Up'
+                    self._view.update_frames(self._player_direction)
+                    self.refresh_view(self._view.out_of_bounds(new_coords))
 
-        if key == "Up":
-            new_coords = (self._player_grid_pos[0], self._player_grid_pos[1]-1)
-            if self._player_grid_pos[1]-1 >= 0:
-                self._player_grid_pos = new_coords
-                self._player_direction = 'Up'
-                self._view.update_frames(self._player_direction)
-                self.refresh_view(self._view.out_of_bounds(new_coords))
+            elif key == "Down":
+                new_coords = (
+                    self._player_grid_pos[0], self._player_grid_pos[1]+1)
+                if self._player_grid_pos[1]+1 <= MAP_SIZE-2:
+                    self._player_grid_pos = new_coords
+                    self._player_direction = 'Down'
+                    self._view.update_frames(self._player_direction)
+                    self.refresh_view(self._view.out_of_bounds(new_coords))
 
-        elif key == "Down":
-            new_coords = (self._player_grid_pos[0], self._player_grid_pos[1]+1)
-            if self._player_grid_pos[1]+1 <= MAP_SIZE-2:
-                self._player_grid_pos = new_coords
-                self._player_direction = 'Down'
-                self._view.update_frames(self._player_direction)
-                print(self._view.out_of_bounds(new_coords))
-                self.refresh_view(self._view.out_of_bounds(new_coords))
+            elif key == "Left":
+                new_coords = (
+                    self._player_grid_pos[0]-1, self._player_grid_pos[1])
+                if self._player_grid_pos[0]-1 >= 0:
+                    self._player_grid_pos = new_coords
+                    self._player_direction = 'Left'
+                    self._view.update_frames(self._player_direction)
+                    self.refresh_view(self._view.out_of_bounds(new_coords))
 
-        elif key == "Left":
-            new_coords = (self._player_grid_pos[0]-1, self._player_grid_pos[1])
-            if self._player_grid_pos[0]-1 >= 0:
-                self._player_grid_pos = new_coords
-                self._player_direction = 'Left'
-                self._view.update_frames(self._player_direction)
-                self.refresh_view(self._view.out_of_bounds(new_coords))
-
-        elif key == "Right":
-            new_coords = (self._player_grid_pos[0]+1, self._player_grid_pos[1])
-            if self._player_grid_pos[0]+1 < MAP_SIZE:
-                self._player_grid_pos = new_coords
-                self._player_direction = 'Right'
-                self._view.update_frames(self._player_direction)
-                self.refresh_view(self._view.out_of_bounds(new_coords))
+            elif key == "Right":
+                new_coords = (
+                    self._player_grid_pos[0]+1, self._player_grid_pos[1])
+                if self._player_grid_pos[0]+1 < MAP_SIZE:
+                    self._player_grid_pos = new_coords
+                    self._player_direction = 'Right'
+                    self._view.update_frames(self._player_direction)
+                    self.refresh_view(self._view.out_of_bounds(new_coords))
 
 
 def main():
